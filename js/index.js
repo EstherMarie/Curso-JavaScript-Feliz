2791b9526f58dc2d920e33b78fcf9cb78a02d640
EstherMarie <24815585+EstherMarie@users.noreply.github.com>
2020-07-23 21:46:32 -0300

    começando o projeto - dia 1 incompleto

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const root = document.querySelector('#root');
+const title = document.createElement('h1');
+title.textContent = 'Dificuldade';
+
+root.append(title);
+
+// BOM - window - informações do navegador como o tamanho da tela por exemplo
+// DOM - document
